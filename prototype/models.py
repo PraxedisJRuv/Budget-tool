3774b7f709b4a7f0dd7843a582b33be424d8ec82
PraxedisJRuv <praxjruv@gmail.com>
2026-07-03 18:26:52 -0600

    add: table sql alchemy

--- prototype/models.py
+++ prototype/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from typing import Optional, Literal, List
 from sqlmodel import Field, SQLModel
 from sqlalchemy import Column, JSON
@@ -7,13 +7,51 @@ from pydantic import BaseModel
 """
 
 """
-class Products_Table(SQLModel):
-    id: int
+class Products_Table(SQLModel, table =True):
+    __tablename__="products"
+    id: int=Field(primary_key=True)
     name: str
     cost: float
     Quantity: Optional[int]=Field(default=None)
     ID_product: int=Field(foreign_key="product.id")
-    ID_session: int=Field(foreign_key="session.id")
+    ID_buy: int=Field(foreign_key="buys.id")
     ID_expense: int=Field(foreign_key="expense.id")
     
+
+class Product_Table(SQLModel, table=True):
+    __tablename__="product"
+    id: Optional[int]= Field(default=None, primary_key=True)
+    name:str
+
+class Buys_Table(SQLModel, table=True):
+    __tablename__="buys"
+    id: Optional[int]=Field(default=None, primary_key=True)
+    date: str=Field(default_factory=lambda: datetime.now().isoformat())
+    cost: Optional[float] =Field(default=None)
+    local: Optional[str] = Field(default=None)
+    business_type: Optional[str]= Field(default=None)
+    amount_products_type: int
+    ID_expense: int=Field(foreign_key="expense.id")
     
+class Expense_Table(SQLModel, table=True):
+    __tablename__="expense"
+    id: Optional[int]=Field(default=None, primary_key=True)
+    date: str=Field(default_factory=lambda: datetime.now().isoformat())
+    amount: float
+    concept: str 
+    description: Optional[str]=Field(default=None)
+class Income_Table(SQLModel, table=True):
+    __tablename__="income"
+    id: Optional[int]=Field(default=None, primary_key=True)
+    date: str=Field(default_factory=lambda: datetime.now().isoformat())
+    amount: float
+    concept: str
+    description: Optional[str]=Field(default=None)
+    
+class Debt_Table(SQLModel, table=True):
+    id: Optional[int]=Field(default=None, primary_key=True)
+    date: str=Field(default_factory=lambda: datetime.now().isoformat())
+    amount: float
+    borrower: str
+    borrowed: str
+    interest: Optional[float]=Field(default=None)

aa99472fdaa5d2260d6b4e295d600e7f173b8bde
PraxedisJRuv <praxjruv@gmail.com>
2026-07-04 20:58:55 -0600

    add: post buy for testing

--- prototype/app.py
+++ prototype/app.py
@@ -12,3 +12,8 @@ create_db()
 @app.post("/buy")
 def record_buy(buy: Buy_Record, session=Depends(get_session)):
     db_buy=Buys_Table.model_validate(buy)
+    session.add(db_buy)
+    #We should add the expense when adding a buy
+    session.commit()
+    session.refresh(db_buy)
+    return db_buy
